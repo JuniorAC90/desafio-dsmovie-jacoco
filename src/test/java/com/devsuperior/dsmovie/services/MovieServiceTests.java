93952565f2cd88349087013805b0822fe7a7bbcf
Aloizio Junior <66444799+JuniorAC90@users.noreply.github.com>
2025-12-29 23:35:13 -0300

    Implemented test findAllShouldReturnPagedMovieDTO

--- src/test/java/com/devsuperior/dsmovie/services/MovieServiceTests.java
+++ src/test/java/com/devsuperior/dsmovie/services/MovieServiceTests.java
@@ -1,18 +1,56 @@
 package com.devsuperior.dsmovie.services;
 
+import com.devsuperior.dsmovie.dto.MovieDTO;
+import com.devsuperior.dsmovie.entities.MovieEntity;
+import com.devsuperior.dsmovie.repositories.MovieRepository;
+import com.devsuperior.dsmovie.tests.MovieFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
 
 @ExtendWith(SpringExtension.class)
 public class MovieServiceTests {
 	
 	@InjectMocks
 	private MovieService service;
-	
+
+	@Mock
+	private MovieRepository repository;
+
+	private MovieEntity entity;
+	private MovieDTO dto;
+	private PageImpl<MovieEntity> page;
+
+	@BeforeEach
+	void setUp() {
+		entity = MovieFactory.createMovieEntity();
+		dto = MovieFactory.createMovieDTO();
+		page = new PageImpl<>(List.of(entity));
+
+		Mockito.when(repository.searchByTitle(any(), (Pageable) any())).thenReturn(page);
+	}
+
 	@Test
 	public void findAllShouldReturnPagedMovieDTO() {
+		Pageable pageable = PageRequest.of(0, 12);
+		Page<MovieDTO> result = service.findAll(entity.getTitle(), pageable);
+
+		Assertions.assertNotNull(result);
+		Assertions.assertEquals(1, result.getSize());
+		Assertions.assertEquals(entity.getTitle(), result.iterator().next().getTitle());
 	}
 	
 	@Test
