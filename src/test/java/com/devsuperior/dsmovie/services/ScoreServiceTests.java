48c3e11446c6d721b290f3349297a0a965e0af86
Aloizio Junior <66444799+JuniorAC90@users.noreply.github.com>
2026-01-06 23:50:46 -0300

    Implemented test saveScoreShouldReturnMovieDTO

--- src/test/java/com/devsuperior/dsmovie/services/ScoreServiceTests.java
+++ src/test/java/com/devsuperior/dsmovie/services/ScoreServiceTests.java
@@ -1,18 +1,77 @@
 package com.devsuperior.dsmovie.services;
 
+import com.devsuperior.dsmovie.dto.MovieDTO;
+import com.devsuperior.dsmovie.dto.ScoreDTO;
+import com.devsuperior.dsmovie.entities.MovieEntity;
+import com.devsuperior.dsmovie.entities.ScoreEntity;
+import com.devsuperior.dsmovie.entities.UserEntity;
+import com.devsuperior.dsmovie.repositories.MovieRepository;
+import com.devsuperior.dsmovie.repositories.ScoreRepository;
+import com.devsuperior.dsmovie.tests.MovieFactory;
+import com.devsuperior.dsmovie.tests.ScoreFactory;
+import com.devsuperior.dsmovie.tests.UserFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
 
 @ExtendWith(SpringExtension.class)
 public class ScoreServiceTests {
 	
 	@InjectMocks
 	private ScoreService service;
+
+	@Mock
+	private ScoreRepository repository;
+
+	@Mock
+	private MovieRepository movieRepository;
+
+	@Mock
+	private UserService userService;
+
+	private UserEntity userEntity;
+
+	private MovieEntity movieEntity;
+
+	private MovieDTO movieDTO;
+
+	private ScoreDTO scoreDTO;
+
+	private ScoreEntity entity;
+
+	private Long existingId;
+
+	@BeforeEach
+	void setUp() {
+		existingId = 1L;
+
+		userEntity = UserFactory.createUserEntity();
+		movieEntity = MovieFactory.createMovieEntity();
+		entity = ScoreFactory.createScoreEntity();
+		scoreDTO = ScoreFactory.createScoreDTO();
+		movieDTO = MovieFactory.createMovieDTO();
+
+		Mockito.when(userService.authenticated()).thenReturn(userEntity);
+		Mockito.when(movieRepository.findById(existingId)).thenReturn(Optional.of(movieEntity));
+		Mockito.when(repository.saveAndFlush(entity)).thenReturn(entity);
+		Mockito.when(movieRepository.save(movieEntity)).thenReturn(movieEntity);
+
+	}
 	
 	@Test
 	public void saveScoreShouldReturnMovieDTO() {
+
+		MovieDTO result = service.saveScore(scoreDTO);
+
+		Assertions.assertNotNull(result);
+		Assertions.assertEquals(existingId, result.getId());
 	}
 	
 	@Test
