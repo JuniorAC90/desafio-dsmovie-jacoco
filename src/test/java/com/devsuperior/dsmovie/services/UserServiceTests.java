4f8f710625513ac15284ea55d6fc83902d3df808
Aloizio Junior <66444799+JuniorAC90@users.noreply.github.com>
2026-01-08 22:54:39 -0300

    Implemented test authenticatedShouldReturnUserEntityWhenUserExists

--- src/test/java/com/devsuperior/dsmovie/services/UserServiceTests.java
+++ src/test/java/com/devsuperior/dsmovie/services/UserServiceTests.java
@@ -1,10 +1,20 @@
 package com.devsuperior.dsmovie.services;
 
+import com.devsuperior.dsmovie.entities.UserEntity;
+import com.devsuperior.dsmovie.repositories.UserRepository;
+import com.devsuperior.dsmovie.tests.UserFactory;
+import com.devsuperior.dsmovie.utils.CustomUserUtil;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration
@@ -13,8 +23,31 @@ public class UserServiceTests {
 	@InjectMocks
 	private UserService service;
 
+	@Mock
+	private UserRepository repository;
+
+	@Mock
+	private CustomUserUtil userUtil;
+
+	private UserEntity entity;
+	private String validUsername;
+
+	@BeforeEach
+	void setUp() {
+		validUsername = "maria@gmail.com";
+		entity = UserFactory.createUserEntity();
+
+		Mockito.when(userUtil.getLoggedUsername()).thenReturn(validUsername);
+		Mockito.when(repository.findByUsername(validUsername)).thenReturn(Optional.of(entity));
+	}
+
 	@Test
 	public void authenticatedShouldReturnUserEntityWhenUserExists() {
+
+		UserEntity result = service.authenticated();
+
+		Assertions.assertNotNull(result);
+		Assertions.assertEquals(validUsername, result.getUsername());
 	}
 
 	@Test
