0d9313a4a6e8cc974f5a6636b3cad5108654cb6d
Aloizio Junior <66444799+JuniorAC90@users.noreply.github.com>
2026-01-10 11:01:18 -0300

    Implemented test loadUserByUsernameShouldReturnUserDetailsWhenUserExists and loadUserByUsernameShouldThrowUsernameNotFoundExceptionWhenUserDoesNotExists

--- src/test/java/com/devsuperior/dsmovie/services/UserServiceTests.java
+++ src/test/java/com/devsuperior/dsmovie/services/UserServiceTests.java
@@ -1,8 +1,10 @@
 package com.devsuperior.dsmovie.services;
 
 import com.devsuperior.dsmovie.entities.UserEntity;
+import com.devsuperior.dsmovie.projections.UserDetailsProjection;
 import com.devsuperior.dsmovie.repositories.UserRepository;
 import com.devsuperior.dsmovie.services.exceptions.ResourceNotFoundException;
+import com.devsuperior.dsmovie.tests.UserDetailsFactory;
 import com.devsuperior.dsmovie.tests.UserFactory;
 import com.devsuperior.dsmovie.utils.CustomUserUtil;
 import org.junit.jupiter.api.Assertions;
@@ -12,10 +14,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @ExtendWith(SpringExtension.class)
@@ -34,15 +39,21 @@ public class UserServiceTests {
 	private UserEntity entity;
 	private String validUsername, invalidUsername;
 
+	private List<UserDetailsProjection> list;
+
 	@BeforeEach
 	void setUp() {
 		validUsername = "maria@gmail.com";
 		invalidUsername = "maria@gmail.com";
 		entity = UserFactory.createUserEntity();
+		list = UserDetailsFactory.createCustomAdminClientUser(validUsername);
 
 		Mockito.when(userUtil.getLoggedUsername()).thenReturn(validUsername);
 		Mockito.when(repository.findByUsername(validUsername)).thenReturn(Optional.of(entity));
 		Mockito.when(repository.findByUsername(invalidUsername)).thenThrow(ResourceNotFoundException.class);
+
+		Mockito.when(repository.searchUserAndRolesByUsername(validUsername)).thenReturn(list);
+		Mockito.when(repository.searchUserAndRolesByUsername(invalidUsername)).thenReturn(new ArrayList<>());
 	}
 
 	@Test
@@ -64,9 +75,19 @@ public class UserServiceTests {
 
 	@Test
 	public void loadUserByUsernameShouldReturnUserDetailsWhenUserExists() {
+
+		UserDetails result = service.loadUserByUsername(validUsername);
+
+		Assertions.assertNotNull(result);
+		Assertions.assertEquals(validUsername, result.getUsername());
+
 	}
 
 	@Test
 	public void loadUserByUsernameShouldThrowUsernameNotFoundExceptionWhenUserDoesNotExists() {
+
+		Assertions.assertThrows(UsernameNotFoundException.class, () -> {
+			UserDetails result = service.loadUserByUsername(validUsername);
+		});
 	}
 }
